bd05f456b5783749c367386755bdf5d0e9460939
yudxp <yudhahamdi@gmail.com>
2022-06-26 12:38:20 +0700

    fix category view as in sketch

--- MC2-17/DiaBeta/Feature/Pages/MealsInput/CategoryViewController.swift
+++ MC2-17/DiaBeta/Feature/Pages/MealsInput/CategoryViewController.swift
@@ -1,102 +1,68 @@
 //
-//  CategoryViewerController.swift
+//  CategoryViewController.swift
 //  DiaBeta
 //
-//  Created by Muhammad Abdul Fattah on 23/06/22.
+//  Created by Yudha Hamdi Arzi on 26/06/22.
 //
 
-import Foundation
 import UIKit
 
-class CategoryViewController: UIViewController{
+class CategoryViewController: UIViewController {
+    var foodCategory = ["Dairy", "Fruit", "Grains", "Protein", "Starch", "Sweets", "Vegetables"]
+//    var categoryDict = ["Dairy": false, "Fruit": false, "Grains": false, "Protein" : false, "Starch" : false, "Sweets" : false, "Vegetables" : false]
+//    var categoryIndex = ["Dairy": 1, "Fruit": 2, "Grains": 3, "Protein" : 4, "Starch" : 5, "Sweets" : 6, "Vegetables" : 7]
+  @IBOutlet weak var categoryTable: UITableView!
   
-  @IBOutlet weak var saveSettingButton: UIButton!
   
-  var category = ["Dairy", "Fruit", "Grains", "Protein", "Starch", "Sweets", "Vegetables"]
-  var categoryDict = ["Dairy": false, "Fruit": false, "Grains": false, "Protein" : false, "Starch" : false, "Sweets" : false, "Vegetables" : false]
-  var categoryIndex = ["Dairy": 1, "Fruit": 2, "Grains": 3, "Protein" : 4, "Starch" : 5, "Sweets" : 6, "Vegetables" : 7]
   
-  var selectedCategoryString = [String]()
-  var selectedCategoryIndex = [Int]()
-  
-  //MARK: - ViewDidLoad
-  override func viewDidLoad()
-  {
-    super.viewDidLoad()
-    saveSettingButton.layer.cornerRadius = 10
-    self.tabBarController?.tabBar.isHidden = true
-  }
-  
-  //MARK: - to Save
-  @IBAction func saveSettingPressed(_ sender: Any)
-  {
-    getSelectedCategory()
-    SharedInfo.shared.category = selectedCategoryString
-    print(SharedInfo.shared.category)
-    print(selectedCategoryString)
-  }
-  
-  //MARK: - Function Helpers
-  private func getSelectedCategory()
-  {
-    selectedCategoryString = categoryDict.allKeys(forValue: true)
-    for string in selectedCategoryString {
-      if categoryIndex[string] != nil {
-        selectedCategoryIndex.append(categoryIndex[string]!)
-      }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
     }
-  }
-  
-  //MARK: - Properties
-  @IBOutlet weak var tableView: UITableView!
-  {
-    didSet {
-      tableView.delegate = self
-      tableView.dataSource = self
-      tableView.isScrollEnabled = false
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
-  }
-  
+    */
+
 }
 
-//MARK: - Table Delegate
-extension CategoryViewController: UITableViewDelegate
-{
-  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    if categoryDict[category[indexPath.row]] == true {
-      categoryDict[category[indexPath.row]] = false
-      tableView.cellForRow(at: indexPath)?.accessoryType = .none
-      
-    } else {
-      categoryDict[category[indexPath.row]] = true
-      tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-    }
+extension CategoryViewController: UITableViewDelegate, UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return foodCategory.count
   }
-}
-
-//MARK: - Table Data Source
-
-extension CategoryViewController:UITableViewDataSource
-{
+  
   func numberOfSections(in tableView: UITableView) -> Int {
     return 1
   }
-
-//  func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-//    return "CATEGORY"
-//  }
-
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    category.count
-  }
-
+  
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-    cell.textLabel?.text = category[indexPath.row]
-  return cell
+    cell.textLabel?.text = foodCategory[indexPath.row]
+    if indexPath.row == 0 {
+      cell.layer.cornerRadius = 10
+      cell.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
+    }
+    else if indexPath.row == foodCategory.count-1 {
+      cell.layer.cornerRadius = 10
+      cell.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
+    }
+    cell.selectionStyle = .none
+    return cell
   }
-
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+  }
+  func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+    tableView.cellForRow(at: indexPath)?.accessoryType = .none
+  }
+  
 }
-
-
-
